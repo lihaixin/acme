43fa89201e79f7c4608d9eb67e276ed6af3b8973
lihaixin <lihaixin@15099.net>
2022-01-10 17:04:46 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ install_cert() {
 mkdir -p /etc/cert/$DOMAIN
 curl https://get.acme.sh | sh
 ~/.acme.sh/acme.sh --set-default-ca  --server  letsencrypt
-~/.acme.sh/acme.sh  --issue --dns dns_cf -d $DOMAIN -d *.$DOMAIN --key-file /etc/cert/$DOMAIN/private.key --fullchain-file /etc/cert/$DOMAIN/fullchain.crt
+~/.acme.sh/acme.sh  --debug --issue --dns dns_cf -d $DOMAIN -d *.$DOMAIN --key-file /etc/cert/$DOMAIN/private.key --fullchain-file /etc/cert/$DOMAIN/fullchain.crt
 ~/.acme.sh/acme.sh --upgrade --auto-upgrade
 }
 
