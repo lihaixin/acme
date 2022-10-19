0d89d50df5e0d7de9f701ae7d4bdbb35968fe98a
lihaixin <lihaixin@15099.net>
2022-10-19 14:26:14 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,49 +36,6 @@ if [ "$DNS" == "dns_cf" ]; then
   ~/.acme.sh/acme.sh --upgrade --auto-upgrade
   fi
 fi
-
-
-if [ "$DNS" == "dns_namecom" ]; then
-: ${Namecom_Username:=$ID}
-: ${Namecom_Token:=$KEY}
-~/.acme.sh/acme.sh  --debug --issue --dns $DNS  -d $DOMAIN -d *.$DOMAIN --key-file /etc/cert/$DOMAIN/private.key --fullchain-file /etc/cert/$DOMAIN/fullchain.crt
-~/.acme.sh/acme.sh --upgrade --auto-upgrade
-fi
-
-if [ "$DNS" == "dns_gd" ]; then
-: ${GD_Key:=$ID}
-: ${GD_Secret:=$KEY}
-~/.acme.sh/acme.sh  --debug --issue --dns $DNS  -d $DOMAIN -d *.$DOMAIN --key-file /etc/cert/$DOMAIN/private.key --fullchain-file /etc/cert/$DOMAIN/fullchain.crt
-~/.acme.sh/acme.sh --upgrade --auto-upgrade
-fi
-
-if [ "$DNS" == "dns_namesilo" ]; then
-: ${Namesilo_Key:=$KEY}
-~/.acme.sh/acme.sh  --debug --issue --dns $DNS --dnssleep 900 -d $DOMAIN -d *.$DOMAIN --key-file /etc/cert/$DOMAIN/private.key --fullchain-file /etc/cert/$DOMAIN/fullchain.crt
-~/.acme.sh/acme.sh --upgrade --auto-upgrade
-fi
-
-if [ "$DNS" == "dns_dp" ]; then
-: ${DP_Id:=$ID}
-: ${DP_Key:=$KEY}
-~/.acme.sh/acme.sh  --debug --issue --dns $DNS -d $DOMAIN -d *.$DOMAIN --key-file /etc/cert/$DOMAIN/private.key --fullchain-file /etc/cert/$DOMAIN/fullchain.crt
-~/.acme.sh/acme.sh --upgrade --auto-upgrade
-fi
-
-if [ "$DNS" == "dns_ali" ]; then
-: ${Ali_Key:=$ID}
-: ${Ali_Secret:=$KEY}
-~/.acme.sh/acme.sh  --debug --issue --dns $DNS -d $DOMAIN -d *.$DOMAIN --key-file /etc/cert/$DOMAIN/private.key --fullchain-file /etc/cert/$DOMAIN/fullchain.crt
-~/.acme.sh/acme.sh --upgrade --auto-upgrade
-fi
-
-if [ "$DNS" == "" ]; then
-  if [ "$DOMAIN" != "" ]; then
-  ~/.acme.sh/acme.sh  --debug --issue  -d $DOMAIN --standalone
-  ~/.acme.sh/acme.sh --installcert -d $DOMAIN --key-file /etc/cert/$DOMAIN/private.key --fullchain-file /etc/cert/$DOMAIN/fullchain.crt
-  ~/.acme.sh/acme.sh --upgrade --auto-upgrade
-  fi
-fi
 }
 
 # 查看证书，没有就自动创建
